e4ec066cedba7fed1b074cebea9c98a8479bf67a
Svetlana <gubinas83@gmail.com>
2020-10-13 18:44:10 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse(n) {
+    if (!Number.isInteger(n)) {
+        return false;
+    }
 
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+
+    let reverseNum = n.toString().split("").reverse().join("");
+
+    return Number(reverseNum);
 }
